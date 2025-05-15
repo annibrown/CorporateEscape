049663a312e406c1cd64f44f402a5964692d9973
SOPHIA BEROUKHIM <beroukhim@chapman.edu>
2025-05-15 15:23:47 -0700

    Updated KeyboardInput

--- Assets/Scripts/KeyboardInput.cs
+++ Assets/Scripts/KeyboardInput.cs
@@ -2,14 +2,36 @@ using UnityEngine;
 
 public class KeyboardInput : MonoBehaviour
 {
+    public OfficeWorker OfficeWorker;
     public Player Player;
-    
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Player.PickupNearbyItems();
         }
+        
+        if (Input.GetKey(KeyCode.W))
+        {
+            // move up
+            OfficeWorker.MoveManually(new Vector2(0, 1));
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            // move down
+            OfficeWorker.MoveManually(new Vector2(0, -1));
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            // move left
+            OfficeWorker.MoveManually(new Vector2(-1, 0));
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            // right
+            OfficeWorker.MoveManually(new Vector2(1, 0));
+        }
+        
     }
 }
+
