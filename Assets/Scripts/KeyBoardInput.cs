dae8b58c65148b61190f7370020c8b4641f18c53
epadilla26 <epadilla@chapman.edu>
2025-05-16 11:09:57 -0700

    Transfered password to its own scene

--- Assets/Scripts/KeyBoardInput.cs
+++ Assets/Scripts/KeyBoardInput.cs
@@ -13,22 +13,22 @@ public class KeyBoardInput : MonoBehaviour
             Player.PickupNearbyItems();
         }
         
-        if (Input.GetKey(KeyCode.W))
+        if (Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
         {
             // move up
             OfficeWorker.MoveManually(new Vector2(0, 1));
         }
-        if (Input.GetKey(KeyCode.S))
+        if (Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.DownArrow))
         {
             // move down
             OfficeWorker.MoveManually(new Vector2(0, -1));
         }
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow))
         {
             // move left
             OfficeWorker.MoveManually(new Vector2(-1, 0));
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow))
         {
             // right
             OfficeWorker.MoveManually(new Vector2(1, 0));
--- Assets/Scripts/OfficeWorker.cs
+++ Assets/Scripts/OfficeWorker.cs
@@ -2,46 +2,42 @@ using UnityEngine;
 
 public class OfficeWorker : MonoBehaviour
 {
-    public SpriteRenderer OfficeWorkerSpriteRenderer;
+    public SpriteRenderer officeWorkerSpriteRenderer;
     
     public void MoveManually(Vector2 direction)
     {
-        //Move(direction);
         Move(direction);
     }
-    
+
     private void KeepOnScreen()
     {
-        OfficeWorkerSpriteRenderer.transform.position =
-            SpriteTools.ConstrainSpriteToScreen(OfficeWorkerSpriteRenderer);
+        officeWorkerSpriteRenderer.transform.position =
+            SpriteTools.ConstrainSpriteToScreen(officeWorkerSpriteRenderer);
     }
-    
+
     public void Move(Vector2 direction)
     {
-        
         FaceCorrectDirection(direction);
-        
+
         float xAmount = GameParameters.OfficeWorkerMoveSpeed * direction.x * Time.deltaTime;
         float yAmount = GameParameters.OfficeWorkerMoveSpeed * direction.y * Time.deltaTime;
-        
-        Vector2 moveAmount = new Vector2(xAmount, yAmount);
-        
-        OfficeWorkerSpriteRenderer.transform.Translate(moveAmount);
 
+        Vector2 moveAmount = new Vector2(xAmount, yAmount);
+
+        officeWorkerSpriteRenderer.transform.Translate(moveAmount);
+        
         KeepOnScreen();
     }
 
     private void FaceCorrectDirection(Vector2 direction)
     {
-        // if move right
         if (direction.x > 0)
         {
-            OfficeWorkerSpriteRenderer.flipX = false;
+            officeWorkerSpriteRenderer.flipX = false;
         }
-        // if nove left
         else if (direction.x < 0)
         {
-            OfficeWorkerSpriteRenderer.flipX = true;
+            officeWorkerSpriteRenderer.flipX = true;
         }
     }
 }
