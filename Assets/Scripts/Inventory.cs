f0bf6bd07cfe4f20ae25d683a33ae27d87752b9b
SOPHIA BEROUKHIM <beroukhim@chapman.edu>
2025-05-18 15:40:59 -0700

    Keypad UI and inventory scripts

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -1,24 +1,35 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory
+public class Inventory : MonoBehaviour
 {
-    public List<Item> Items;
-    
+    // Event fired whenever the inventory changes
+    public event Action OnInventoryChanged;
+
+    private List<Item> items = new List<Item>();
+    public IReadOnlyList<Item> Items => items;
+
+    private void Awake()
+    {
+        // nothing else needed here yet
+    }
+
     public void Pickup(Item item)
     {
-        Items.Add(item);
+        items.Add(item);
         item.GetPickedUp();
+        OnInventoryChanged?.Invoke();
     }
 
     public void Drop(Item item, Vector3 playerPosition)
     {
-        Items.Remove(item);
-        item.GetDropped(playerPosition);
+        if (items.Remove(item))
+        {
+            item.GetDropped(playerPosition);
+            OnInventoryChanged?.Invoke();
+        }
     }
 
-    public bool InInventory(Item item)
-    {
-        return Items.Contains(item);
-    }
+    public bool InInventory(Item item) => items.Contains(item);
 }
