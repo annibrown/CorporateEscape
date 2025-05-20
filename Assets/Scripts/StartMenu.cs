7f8e13e1f99bb5963a755fee1575182ce99e4b24
Emily Pham <empham@chapman.edu>
2025-05-20 08:31:52 -0700

    Added opening dialogue

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -5,7 +5,8 @@ public class StartMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Game-MainRoom");
+        //SceneManager.LoadScene("Game-MainRoom");
+        SceneManager.LoadScene("Game-OpeningDialogue");
     }
 
     public void QuitGame()
